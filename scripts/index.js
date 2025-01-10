476e00df3d2fe7d72f6cb2b904e13125e4b2af03
Aliev Amrah <aar2103@yandex.ru>
2025-01-10 16:14:49 +0300

    after-rewiew

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,30 @@
 const placesContainer = document.querySelector('#card-template').content; 
 const placesList = document.querySelector('.places__list');
 
-function deleteBtn(button) {
-  button.addEventListener('click', function() {
-    button.closest('.places__item').remove();
-  })
+function createCard(element, deleteCard) {
+   // инициалируем переменную, которая клонирует template
+   const place = placesContainer.querySelector('.places__item').cloneNode(true);
+   // наполняем содержимым
+   place.querySelector('.card__image').src = element.link;
+   place.querySelector('.card__image').alt = element.alt;
+   place.querySelector('.card__title').textContent = element.name;
+
+   const deleteC= place.querySelector('.card__delete-button');
+   deleteC.addEventListener('click', deleteCard);
+   
+   return place;
+}
+
+function deleteCard() {
+  const card = document.querySelector('.card__delete-button').closest('.places__item')
+  card.remove();
 }
 
 
-initialCards.forEach((function (element) {
-  // инициалируем переменную, которая клонирует template
-  const place = placesContainer.querySelector('.places__item').cloneNode(true);
-  
-  // наполняем содержимым
-  place.querySelector('.card__image').src = element.link;
-  place.querySelector('.card__title').textContent = element.name;
-
-  // отображаем на странице
+function renderCard(place, placesList) {
   placesList.append(place);
+}
 
-  // удаление карточки
-  const Buttons = document.querySelectorAll('.card__delete-button');
-  Buttons.forEach(deleteBtn);
-}))
+initialCards.forEach((element) => {
+  renderCard(createCard(element, deleteCard), placesList)
+})
